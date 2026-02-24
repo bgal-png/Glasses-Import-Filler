c68b7177e7b59b579d5762021bdcf2b08866c3a2
BobHub <bgal@alensa.eu>
2026-02-24 14:27:14 +0100

    database setup

--- db_test.py
+++ db_test.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 
 # ⚠️ Paste your Supabase Connection String here
 # Make sure it starts with 'postgresql://' (NOT postgresql+psycopg2:// yet)
-DB_URL = "postgresql://postgres.nxlwkzgfcmzsbogcenyi:lzAxicGTtsq1iEqB@aws-1-eu-central-1.pooler.supabase.com:5432/postgres"
+DB_URL = "postgresql://postgres.nxlwkzgfcmzsbogcenyi:YQe2oULo6y6WXOZN@aws-1-eu-central-1.pooler.supabase.com:5432/postgres"
 
 def test_connection():
     print("⏳ Attempting to connect to the Vault...")
